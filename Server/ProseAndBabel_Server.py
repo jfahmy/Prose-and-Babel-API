f76f5e88559aaf594d59369c5f80b6d1895c3d48
jfahmy <jacqueline.fahmy0.o@gmail.com>
2019-01-02 15:13:34 -0800

    Add basic proto and server file with haiku request defined

---
+++ Server/ProseAndBabel_Server.py
@@ -0,0 +1,29 @@
+from concurrent import futures
+import time
+
+import grpc
+
+import ProseAndBabel_pb2
+import ProseAndBabel_pb2_grpc
+
+_ONE_DAY_IN_SECONDS = 60 * 60 * 24
+
+class ProseAndBabel(ProseAndBabel_pb2_grpc.ProseAndBabelServicer):
+
+    def GetHaiku(self, request, context):
+        return ProseAndBabel_pb2.Haiku(prose="this is a test")
+
+
+def serve():
+ server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+ ProseAndBabel_pb2_grpc.add_ProseAndBabelServicer_to_server(ProseAndBabel(), server)
+ server.add_insecure_port('[::]:50050')
+ server.start()
+ try:
+   while True:
+     time.sleep(_ONE_DAY_IN_SECONDS)
+ except KeyboardInterrupt:
+   server.stop(0)
+
+if __name__ == '__main__':
+ serve()
